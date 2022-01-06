3b68c08ed2fb91a1c033ad67a3a1928d4408c389
Supraja Chandrasekaran <supraja.chandrasekaran@openweaver.com>
2022-01-06 12:35:32 +0530

    First_Commit

---
+++ cryptoboys.bat
@@ -0,0 +1,8 @@
+git clone https://github.com/SuprajaChandrasekaran/cryptoboys-nft-marketplace.git
+cd cryptoboys-nft-marketplace
+npm install
+npm install -g truffle
+npm i ganache-cli
+truffle compile
+truffle migrate
+truffle test
